0619fe2bb506ea39989c3d27df682c08c779b839
andrikoz <andrew.gounaris@theblueground.com>
2025-06-26 13:06:03 +0300

    Add error page that redirects

--- error.vue
+++ error.vue
@@ -1,5 +1,14 @@
-<script setup>
+<script setup lang="ts">
+import type { NuxtError } from '#app'
 import errorImage from './assets/images/404.svg?raw'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+if (props.error?.statusCode === 404) {
+  clearError({ redirect: 'https://theblueground.com' })
+}
 </script>
 
 <template>
